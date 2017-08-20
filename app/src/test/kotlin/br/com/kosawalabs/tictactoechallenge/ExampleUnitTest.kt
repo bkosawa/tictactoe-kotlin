59f436a49794181106921078c30d72a632384e1f
Bruno Kosawa <bkosawa@gmail.com>
2017-08-20 10:05:11 -0300

    Creating MainActivity and layout

---
+++ app/src/test/kotlin/br/com/kosawalabs/tictactoechallenge/ExampleUnitTest.kt
@@ -0,0 +1,13 @@
+package br.com.kosawalabs.tictactoechallenge
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ExampleUnitTest {
+    @Test
+    @Throws(Exception::class)
+    fun addition_isCorrect() {
+        assertEquals(4, (2 + 2).toLong())
+    }
+}
